795a0e7278fbba0a01a57bd0cd11b3cca21c7a63
Levi Payne <Levi Payne>
2017-01-25 06:56:25 -0500

    Created Models and few basic tests

--- budgetapp/models.py
+++ budgetapp/models.py
@@ -1,2 +1,58 @@
-from django.db import models
+import datetime
+import moneyed
 
+from djmoney.models.fields import MoneyField
+from django.db import models
+from datetime import date
+
+
+class Budget(models.Model):
+	month = models.DateField()
+
+class CategoryGroup(models.Model):
+	name = models.CharField(max_length=100)
+
+class Category(models.Model):
+	name = models.CharField(max_length=100)
+	group = models.ForeignKey(CategoryGroup, on_delete=models.CASCADE)
+
+class CategoryBudget(models.Model):
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+	limit = MoneyField(max_digits=10, decimal_places=2, default=0, default_currency='USD')
+	spent = MoneyField(max_digits=10, decimal_places=2, default=0, default_currency='USD')
+
+	def get_money_left(self):
+		return self.limit - self.spent
+
+class Transaction(models.Model):
+	amount = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
+	recipient = models.CharField(max_length=100)
+	category_budget = models.ForeignKey(CategoryBudget, on_delete=models.CASCADE)
+	date = models.DateField()
+
+class Income(models.Model):
+	name = models.CharField(max_length=100)
+	amount = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
+	budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+
+class Goal(models.Model):
+	name = models.CharField(max_length=100)
+	goal_amount = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
+	progress = MoneyField(max_digits=10, decimal_places=2, default=0, default_currency='USD')
+
+	def is_met(self):
+		return self.progress >= self.goal_amount
+
+	class Meta:
+		abstract = True
+
+class LongTermGoal(Goal):
+	due_date = models.DateField()
+
+	def is_past_due(self):
+		return date.today() > self.due_date
+
+class BudgetGoal(Goal):
+	budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+	long_term_goal = models.ForeignKey(LongTermGoal, on_delete=models.CASCADE, null=True)
--- budgetapp/tests.py
+++ budgetapp/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from datetime import date
+from .models import Budget, CategoryGroup, Category, CategoryBudget, Transaction, Income, Goal, LongTermGoal, BudgetGoal
+
+class ModelMethodTests(TestCase):
+
+	def test_is_past_due_with_past_date(self):
+		"""
+		Should return true
+		"""
+		temp_date = date.today() - datetime.timedelta(days=30)
+		goal = LongTermGoal(due_date=temp_date)
+		self.assertIs(goal.is_past_due(), True)
+
+	def test_is_past_due_with_future_date(self):
+		"""
+		Should return false
+		"""
+		temp_date = date.today() + datetime.timedelta(days=30)
+		goal = LongTermGoal(due_date=temp_date)
+		self.assertIs(goal.is_past_due(), False)
+
+	def test_is_past_due_with_current_date(self):
+		"""
+		Should return false
+		"""
+		temp_date = date.today()
+		goal = LongTermGoal(due_date=temp_date)
+		self.assertIs(goal.is_past_due(), False)
